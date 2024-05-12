688c6f7213e5463d0ca258a43ef9d20994c1fc66
Jacobi <jakub.sochacki@wp.pl>
2024-05-12 19:41:32 +0200

    add batch size to hp

--- src/hyperparameters.py
+++ src/hyperparameters.py
@@ -5,7 +5,10 @@ from dataclasses import dataclass
 class Hyperparameters:
     num_epochs: int = 10
     learning_rate: float = 0.001
+    batch_size: int = 64
 
     wandb_config = {
             "num_epochs": num_epochs,
+            "learning_rate": learning_rate,
+            "batch_size": batch_size
     }
